329c8775d5426960040d6f841d129ecd57324a3a
inthaider <syedjibran.haider@richmond.edu>
2023-10-20 14:15:40 -0400

    Redefine `train_rnn_model` with additional print statements for each epoch, training loop, and test loop

--- src/models/rnn_model.py
+++ src/models/rnn_model.py
@@ -38,8 +38,9 @@ class WalkDataset(Dataset):
     def __getitem__(self, idx):
         return self.X[idx], self.Y[idx]
 
+# Define the training function
 
-def train_rnn_model(X_train, Y_train, X_test, Y_test, input_size, hidden_size, output_size, num_epochs, batch_size, learning_rate, device, batch_first=True):
+def train_rnn_model(X_train, Y_train, X_test, Y_test, input_size, hidden_size, output_size, num_epochs, batch_size, learning_rate, device, batch_first=True, print_freq=10):
     # Create the dataset and data loader
     train_dataset = WalkDataset(X_train, Y_train)
     test_dataset = WalkDataset(X_test, Y_test)
@@ -50,38 +51,46 @@ def train_rnn_model(X_train, Y_train, X_test, Y_test, input_size, hidden_size, o
 
     # Define the model, loss function, and optimizer
     model = RNN(input_size=input_size, hidden_size=hidden_size,
-                output_size=output_size, batch_first=True).to(device)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+                output_size=output_size, batch_first=batch_first).to(device)
+    criterion = nn.CrossEntropyLoss()  # Using CrossEntropyLoss as the loss function
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate)  # Using SGD as the optimizer
 
-    # Flags to control logging
-    log_invalid_loss = True
-    log_invalid_grad = True
-
-    # Train the model
+    ################################
+    # Train and evaluate the model #
+    ################################
     for epoch in range(num_epochs):
-        model.train()
-        running_loss = 0.0
+        print(f"Epoch {epoch+1}/{num_epochs}\n-------------------------------")
 
-        # Initialize sum of squared gradients
+
+        #################
+        # Training loop #
+        #################
+        size_train = len(train_loader.dataset)
+        # Set the model to training mode - important for batch normalization and dropout layers
+        # This is best practice, but is it necessary here in this situation?
+        model.train()
+        # Initialize running loss & sum of squared gradients and parameters
+        running_loss = 0.0
         sum_sq_gradients = 0.0
         sum_sq_parameters = 0.0
-
         for i, (inputs, labels) in enumerate(train_loader):
-            inputs, labels = inputs.to(device), labels.to(device)
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
+            inputs, labels = inputs.to(device), labels.to(device)  # Move tensors to device, e.g. GPU
+            optimizer.zero_grad()  # Zero the parameter gradients
+            
+            # Compute predicted output and loss
+            outputs = model(inputs)  # Forward pass
+            loss = criterion(outputs, labels)  # Compute loss
+
+            # Backpropagation
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
 
             # Debugging: Check for NaN or inf in loss
             if np.isnan(loss.item()) or np.isinf(loss.item()):
-                if log_invalid_loss:
-                    logging.warning(
-                        f"First occurrence of invalid loss {loss.item()} at iteration {i} of epoch {epoch}. Further warnings will be suppressed.")
-                    log_invalid_loss = False
+                logging.warning(
+                    f"First occurrence of invalid loss {loss.item()} at iteration {i} of epoch {epoch}. Further warnings will be suppressed.")
                 continue
-
-            loss.backward()
 
             # Debugging: Check for NaN or inf in gradients
             for name, param in model.named_parameters():
@@ -89,13 +98,8 @@ def train_rnn_model(X_train, Y_train, X_test, Y_test, input_size, hidden_size, o
                     grad_check = torch.sum(torch.isnan(
                         param.grad)) + torch.sum(torch.isinf(param.grad))
                     if grad_check > 0:
-                        if log_invalid_grad:
-                            logging.warning(
-                                f"First occurrence of invalid gradient in {name} at iteration {i} of epoch {epoch}. Further warnings will be suppressed.")
-                            log_invalid_grad = False
-
-            optimizer.step()
-            running_loss += loss.item()
+                        logging.warning(
+                            f"First occurrence of invalid gradient in {name} at iteration {i} of epoch {epoch}. Further warnings will be suppressed.")
 
             # Debugging: Monitor sum of squared gradients and parameters
             for name, param in model.named_parameters():
@@ -105,29 +109,47 @@ def train_rnn_model(X_train, Y_train, X_test, Y_test, input_size, hidden_size, o
                 if param.data is not None:
                     sum_sq_parameters += torch.sum(param.data ** 2).item()
 
+            # Print loss every print_freq iterations
+            if (i+1) % print_freq == 0:
+                loss, current_iter = loss.item(), (i + 1) * len(inputs)  # loss and current iteration
+                print(f"Loss: {loss:>7f}  [{current_iter:>5d}/{size_train:>5d}]")
+
         # Log sum of squared gradients and parameters after each epoch
         logging.info(
             f"Epoch {epoch+1}: Sum of squared gradients: {sum_sq_gradients:.4f}, Sum of squared parameters: {sum_sq_parameters:.4f}")
-
+        
+        # Calculate average loss over all batches
         train_loss = running_loss / len(train_loader)
+        print(f"Train Error: \n Avg loss: {train_loss:>8f} \n")
 
+        #############
+        # Test loop #
+        #############
+        # Set the model to evaluation mode - important for batch normalization and dropout layers
+        # This is best practice, but is it necessary here in this situation?
         model.eval()
+        # Initialize running loss & sum of squared gradients and parameters
         running_loss = 0.0
         correct = 0
         total = 0
+        # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
+        # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
         with torch.no_grad():
             for i, (inputs, labels) in enumerate(test_loader):
-                inputs, labels = inputs.to(device), labels.to(device)
-                outputs = model(inputs)
-                loss = criterion(outputs, labels)
-                running_loss += loss.item()
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
+                inputs, labels = inputs.to(device), labels.to(device)  # Move tensors to device, e.g. GPU
+                outputs = model(inputs)  # Forward pass
+                loss = criterion(outputs, labels)  # Compute loss
+                running_loss += loss.item()  # Accumulate loss
+                _, predicted = torch.max(outputs.data, 1)  # Get predicted class
+                total += labels.size(0)  # Accumulate total number of samples
+                correct += (predicted == labels).sum().item()  # Accumulate number of correct predictions
+        
+        # Calculate average loss and accuracy over all batches
         test_loss = running_loss / len(test_loader)
         test_acc = correct / total
 
-        logging.info(
-            f'Epoch {epoch+1}/{num_epochs}: Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.4f}')
+        print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+        # print(
+        #     f'Epoch {epoch+1}: Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.4f}')
 
     return model
