1e9f0020c90e793af88c7686b586ecf76d19a5f1
faizan-dogar <dogar.faizan@gmail.com>
2023-10-31 11:11:41 -0400

    Added code that tracks testing indices and plots predicted probs for test set

--- src/RNN Implementation.py
+++ src/RNN Implementation.py
@@ -53,6 +53,8 @@ df = df[['Frame', 'Fdis', 'FdisF', 'FdisL', 'Wdis', 'WdisF',
 
 df.replace([np.inf, -np.inf], np.nan, inplace=True)
 
+df = df.reset_index(drop=True)
+
 '''
 def create_sequences(data, sequence_length=5):
     x, y = [], []
@@ -70,10 +72,11 @@ def create_sequences(data, sequence_length=5):
     return np.array(x), np.array(y)
 '''
 
-def create_sequences(data, sequence_length=5):
+def create_sequences(data, sequence_length=5, start_index = 0):
     n = len(data) - sequence_length
     x = np.empty((n, sequence_length, data.shape[1]))
     y = np.empty(n)
+    indices = np.empty(n, dtype=int)
     
     valid_idx = 0
     for i in range(n):
@@ -84,23 +87,27 @@ def create_sequences(data, sequence_length=5):
         if not np.isnan(sequence).any() and not np.isnan(target):
             x[valid_idx] = sequence
             y[valid_idx] = target
+            indices[valid_idx] = start_index + i + sequence_length
             valid_idx += 1
             
     # Trim the arrays to the size of valid sequences
     x = x[:valid_idx]
     y = y[:valid_idx]
-    return x, y
+    indices = indices[:valid_idx]
+    return x, y, indices
 
 X_train, Y_train = [], []
 X_test, Y_test = [], []
 files = df['file'].unique()
 
+test_indices = []
 for i, file in enumerate(files):
     print(i, file)
+    file_df = df[df['file'] == file]
     file_data = df[df['file'] == file].drop(['Frame', 'file'], axis=1).values
     
     # Create sequences for each file
-    x, y = create_sequences(file_data)
+    x, y, idx = create_sequences(file_data, start_index=file_df.index.min())
     
     # Calculate the split index
     train_size = int(len(x) * 2/3)
@@ -110,6 +117,10 @@ for i, file in enumerate(files):
     Y_train.extend(y[:train_size])
     X_test.extend(x[train_size:])
     Y_test.extend(y[train_size:])
+    
+    # Extract the test indices corresponding to Y_test
+    test_indices.extend(idx[train_size:])
+    
 
 X_train = np.array(X_train)
 X_train = X_train[:, :, :-1]
@@ -130,7 +141,7 @@ ros = RandomOverSampler(random_state=42)
 X_train_resampled, Y_train_resampled = ros.fit_resample(X_train.reshape(X_train.shape[0], -1), Y_train)
 
 # Reshape X_train back to its original shape
-X_train_resampled = X_train_resampled.reshape(-1, 5, 19)
+X_train_resampled = X_train_resampled.reshape(-1, 3, 19)
 
 print("Original dataset shape:", X_train.shape, Y_train.shape)
 print("Resampled dataset shape:", X_train_resampled.shape, Y_train_resampled.shape)
@@ -290,6 +301,105 @@ for epoch in range(num_epochs):
     print(f'Epoch {epoch+1}/{num_epochs}: Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.4f}, Test F1: {test_f1:.4f}, Test PR AUC: {test_pr_auc:.4f}')
 
 
+# Plotting predicted probabilities
+import torch.nn.functional as F
+all_preds = []
+all_preds_probs = []
+all_labels = []
+with torch.no_grad():
+    for i, (inputs, labels) in enumerate(test_loader):
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        #loss = criterion(outputs, labels)
+        #running_loss += loss.item()
+        _, predicted = torch.max(outputs.data, 1)
+        #total += labels.size(0)
+        #correct += (predicted == labels).sum().item()
+        
+        #print(outputs.data, torch.max(outputs.data,1))
+        
+        
+        
+        probabilities = F.softmax(outputs, dim=1)
+        prob_of_class_1 = probabilities[:, 1]
+        #print(prob_of_class_1)
+        
+        all_preds.extend(predicted.cpu().numpy())
+        all_labels.extend(labels.cpu().numpy())
+        all_preds_probs.extend(prob_of_class_1.cpu().numpy())
+
+
+import pandas as pd
+
+# Convert the lists to a DataFrame
+merged_df = pd.DataFrame({
+    'index': test_indices,
+    'y_test': all_labels,
+    'y_pred': all_preds_probs
+})
+
+plot_df = pd.merge(df, merged_df, left_index=True, right_on='index', how='right')
+
+plot_df = plot_df.sort_values(by=["file", "Frame"])
+
+########## Flagging frames that are 5 seconds prior of a backing event
+def flag_frames(group):
+    group = group.reset_index(drop=True)  # Reset index for proper index-based operations
+    group['flag'] = 0  # Initialize 'flag' column with zeros
+    start_indices = group.index[group['start_walk'] == 1]  # Indices where 'start_frame' equals 'Frame'
+    for idx in start_indices:
+        # Flag frames that are within a 40-frame value distance before the current frame
+        mask = (group['Frame'] >= group.loc[idx, 'Frame'] - 200) & (group['Frame'] <= group.loc[idx, 'Frame'])
+        group.loc[mask, 'flag'] = 1
+    return group
+
+# Apply the custom function to each file group
+plot_df = plot_df.groupby('file').apply(flag_frames).reset_index(drop=True)
+
+# Create a new column that marks when the flag changes
+plot_df['flag_change'] = plot_df['flag'].diff().ne(0)
+
+# Assign a unique group number to each change in flag
+plot_df['group_number'] = plot_df['flag_change'].cumsum()
+
+# Cleanup: remove the 'flag_change' column
+plot_df.drop(columns=['flag_change'], inplace=True)
+
+# First, create a mask where Frame equals start_frame
+start_frame_mask = (plot_df['start_walk'] == 1)
+
+# Then, for each group, get the value of Frame where Frame equals start_frame
+plot_df['backing_frame'] = plot_df.loc[start_frame_mask, 'Frame']
+
+# Forward fill 'backing_frame' within each group
+#plot_df['backing_frame'] = plot_df.groupby('group_number')['backing_frame'].ffill()
+
+# Backward fill 'backing_frame' within each group
+plot_df['backing_frame'] = plot_df.groupby('group_number')['backing_frame'].bfill()
+
+# Calculate delta frmes
+plot_df['delta_frames'] = plot_df['Frame'] - plot_df['backing_frame']
+
+
+# Plot of predicted probabilities 1 second before and after a true backing event
+plot_df_2 = plot_df[plot_df['flag']==1]
+plot_df_2['delta_frames'] = plot_df_2['Frame'] - plot_df_2['backing_frame']
+plot_df_2 = plot_df_2[['file', 'Frame', 'start_walk', 'backing_frame', 'delta_frames', 'y_test', 'y_pred']]
+
+
+mean_df = plot_df_2.groupby('delta_frames')[['y_pred']].mean().reset_index()
+
+# Plot of predictions within 200 frames (5 seconds
+# Assuming mean_df is your DataFrame and it has columns 'delta_frames' and 'y_pred'
+plt.figure(figsize=(10,6))  # Create a new figure with a specific size (optional)
+plt.plot(mean_df['delta_frames'], mean_df['y_pred'])  # Plot 'y_pred' against 'delta_frames'
+
+# Optional: add labels and title
+plt.xlabel('Delta Frames')
+plt.ylabel('y_pred')
+plt.title('y_pred vs Delta Frames')
+
+plt.show()  # Display the plot)
 
 
 
