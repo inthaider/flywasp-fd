0777c8e6d5f8368c1cda7790774470eb87bd522b
inthaider <syedjibran.haider@richmond.edu>
2023-11-14 14:41:08 -0500

    Refactor DataPreprocessor class to improve code structure and readability

--- src/data_preprocess/preprocessing.py
+++ src/data_preprocess/preprocessing.py
@@ -18,22 +18,20 @@ Classes:
 
 Example:
     To preprocess a dataset using the DataPreprocessor class:
-
-    >>> preprocessor = DataPreprocessor(pickle_path='path/to/data.pkl')
-    >>> df_processed = preprocessor.preprocess_data(save_data=True)
+TODO: Fix these examples:
+        >>> preprocessor = DataPreprocessor(pickle_path='path/to/data.pkl')
+        >>> df_processed = preprocessor.preprocess_data(save_data=True)
 
 Note:
     This module expects the raw data to be in a Pandas DataFrame format
     and to be available at the specified pickle path.
 """
 
-import hashlib
 import logging
 from datetime import datetime
-from pathlib import Path
 
-import numpy as np
 import pandas as pd
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -45,19 +43,10 @@ class DataPreprocessor:
     Attributes:
         df_raw (pd.DataFrame): The raw DataFrame.
         df (pd.DataFrame): The processed DataFrame.
-        pickle_path (str): The path to the pickled file.
         raw_data_id (str): The ID of the raw data.
-        save_data (bool): Whether to save the processed data to a
-            pickled file.
-        raw_data_path (str): The path to the raw data.
-        interim_data_path (str): The path to the interim data.
-        processed_data_path (str): The path to the processed data.
         timestamp (str): The timestamp of the processed data.
 
     Methods:
-        load_data(): Loads the DataFrame from a pickled file.
-        save_processed_data(): Saves the processed data to a pickled
-            file.
         drop_columns(columns_to_drop): Drops the specified columns from
             the DataFrame.
         specific_rearrange(col_to_move, ref_col): Moves a column to be
@@ -70,123 +59,19 @@ class DataPreprocessor:
             based on conditions of existing columns.
         handle_infinity_and_na(): Replaces infinite and NaN values in
             the DataFrame with forward/backward filled values.
-        preprocess_data(save_data): Performs preprocessing steps on the
-            DataFrame.
+        preprocess_data(): High-level method to orchestrate
+            preprocessing steps on the DataFrame.
     """
 
     def __init__(
         self,
-        pickle_path=None,
+        df_raw: pd.DataFrame,
         raw_data_id: str = "ff-mw",
-        save_data: bool = False,
     ):
-        """
-        Initializes a new instance of the DataPreprocessor class.
-
-        Args:
-            pickle_path (str, optional): The path to the pickled file.
-                Defaults to None.
-            raw_data_id (str, optional): The ID of the raw data.
-                Defaults to "ff-mw".
-            save_data (bool, optional): Whether to save the processed
-                data to a pickled file. Defaults to False.
-        """
-        self.df_raw = None
-        self.df = None
-        self.pickle_path = pickle_path
+        self.df_raw = df_raw
+        self.df = df_raw.copy()
         self.raw_data_id = raw_data_id
-        self.save_data = save_data
-        self.raw_data_path = None
-        self.interim_data_path = self.pickle_path
-        self.processed_data_path = None
         self.timestamp = datetime.now().strftime("%Y%m%d_%H%M")
-
-    def load_data(self):
-        """
-        Loads the DataFrame from a pickled file.
-
-        Returns:
-            pd.DataFrame: The loaded DataFrame.
-
-        Raises:
-            ValueError: If an invalid pickle path is provided.
-            Exception: If an error occurs while loading data.
-        """
-        # Logging statement to indicate the start of loading data
-        logger.debug("\nLoading data in DataPreprocessor -> load_data() ...")
-        try:
-            if self.pickle_path:
-                logger.debug(f"Loading data from {self.pickle_path}...")
-                self.df_raw = pd.read_pickle(self.pickle_path)
-                # Make a copy of the loaded DataFrame
-                self.df = self.df_raw.copy()
-                logger.debug(f"Data loaded from {self.pickle_path}.")
-                return self.df_raw
-            else:
-                raise ValueError(
-                    "\nVALUE ERROR: Provide a valid pickle path.\n"
-                )
-        except Exception as e:
-            logger.error(f"\nERROR loading data: {e}\n")
-            raise
-
-    def save_processed_data(self) -> str:
-        """
-        Saves the processed data to a pickled file.
-
-        Returns:
-            str: The path to the saved file.
-
-        Raises:
-            Exception: If an error occurs while saving processed data.
-        """
-        # Logging statement to indicate the start of saving processed
-        # data
-        logger.debug(
-            "\nSaving processed data in DataPreprocessor -> "
-            "save_processed_data() ..."
-        )
-        try:
-            # Create the directory for the processed data if it doesn't
-            # exist
-            processed_data_dir = Path(f"data/processed/{self.raw_data_id}")
-            processed_data_dir.mkdir(parents=True, exist_ok=True)
-
-            #
-            # Generate a hash based on DataFrame metadata and some
-            # sampling
-            #
-            logger.debug("Generating hash for processed data...")
-            # Extracting the shape, columns and a sample of the
-            # dataframe
-            df_summary = (
-                f"{self.df.shape}"
-                f"{self.df.columns}"
-                f"{self.df.sample(n=10, random_state=1)}"
-            )
-            # Generating the hash using the md5 algorithm based on the
-            # dataframe summary
-            processed_data_hash = hashlib.md5(df_summary.encode()).hexdigest()
-            logger.debug("Processed data hashed.")
-
-            # Construct the output file path
-            self.processed_data_path = (
-                processed_data_dir
-                / f"{self.timestamp}_processed_data_{processed_data_hash}.pkl"
-            )
-
-            # Save the processed data to the output file
-            logger.debug(
-                f"Saving processed data to {self.processed_data_path}..."
-            )
-            self.df.to_pickle(self.processed_data_path)
-            logger.debug(
-                f"Processed data saved to {self.processed_data_path}.\n"
-            )
-            return str(self.processed_data_path)
-        except Exception as e:
-            logger.error(f"\nERROR saving processed data: {e}\n")
-            raise
 
     def drop_columns(self, columns_to_drop):
         """
@@ -204,6 +89,7 @@ class DataPreprocessor:
             f"\nDropping columns {columns_to_drop} in DataPreprocessor -> "
             "drop_columns() ..."
         )
+        
         try:
             self.df.drop(columns_to_drop, axis=1, inplace=True)
             # Logging statement to indicate the end of dropping columns
@@ -365,13 +251,9 @@ class DataPreprocessor:
             logger.error(f"\nERROR handling infinite and NaN values: {e}\n")
             raise
 
-    def preprocess_data(self, save_data: bool = False):
+    def preprocess_data(self):
         """
         Performs preprocessing steps on the DataFrame.
-
-        Args:
-            save_data (bool, optional): Whether to save the processed
-                data to a pickled file. Defaults to None.
 
         Returns:
             pd.DataFrame: The processed DataFrame.
@@ -379,11 +261,6 @@ class DataPreprocessor:
         # Add logging statements to indicate the start of preprocessing
         # as part of the DataPreprocessor class
         logger.info("\n\nPreprocessing data (in DataPreprocessor class)...")
-
-        # Set the value of self.save_data to the value of save_data if
-        # save_data is not None
-        if save_data is not False:
-            self.save_data = save_data
 
         # Drop the 'plot' column
         self.drop_columns(["plot"])
@@ -428,12 +305,6 @@ class DataPreprocessor:
                 "start_walk",
             ]
         )
-
-        # Save the processed data to a pickled file if self.save_data is
-        # True.
-        if self.save_data:
-            logger.info("!! save_data is True !!")
-            self.save_processed_data()
 
         # Print the shape of the dataframe and its columns using the
         # print module.
